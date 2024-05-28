ad13d09ce6ed43c3d47b2d779a10c0cef33ea93c
vishvas patel <vishvaspatel1410@gmail.com>
2024-05-29 03:32:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# online-shoping-system
+ An online shopping system allows users to browse products, add items to their cart, and complete purchases through an integrated payment gateway. It provides a seamless shopping experience, handling everything from user registration to order processing.
---
+++ admin/admin/includes/db.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = "localhost:3308";
+$username = "root";
+$password = "";
+$db = "onlineshop";
+
+
+// Create connection
+$con = mysqli_connect($servername, $username, $password,$db);
+
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+?>
